cfa669df58051088b4f6f18ff8375b55a59ecd33
Dani7867 <dsheikh672@gmail.com>
2025-07-22 15:51:52 +0530

    feat(order): initialize order-service-simulator

---
+++ order-service-simulator/order-service-simulator/src/test/java/com/danish/orderservicesimulator/OrderServiceSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danish.orderservicesimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceSimulatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
